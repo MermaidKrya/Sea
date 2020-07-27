0c58d58a97593af6ad71bbb5b4c94b60e9d2ca01
Mermaid <aasandler@yandex.ru>
2020-07-27 17:07:46 +0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public partial class Form1 : Form
+    {
+        public const int mapSize = 10;
+        public int cellSize = 30;
+        public string alphabet = "АБВГДЕЖЗИК";
+
+        public int[,] myMap = new int[mapSize, mapSize];
+
+        public Button[,] myButtons = new Button[mapSize, mapSize];
+        public static bool isPlaying = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Морской бой";
+            Init();
+        }
+
+        public void Init()
+        {
+            isPlaying = false;
+            CreateMaps();
+        }
+
+        public void CreateMaps()
+        {
+            this.Width = mapSize * 2 * cellSize + 50;
+            this.Height = (mapSize + 3) * cellSize + 20;
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    myMap[i, j] = 0;
+
+                    Button button = new Button();
+                    button.Location = new Point(j * cellSize, i * cellSize);
+                    button.Size = new Size(cellSize, cellSize);
+                    button.BackColor = Color.White;
+                    GoogleApi myApi = new GoogleApi();
+                            if (GoogleApi.array[i, j] != 0)
+                            {
+                                //myMap[i, j] = 1;
+                                button.BackColor = Color.Red;
+                            }
+                            else
+                            {
+                                button.BackColor = Color.Gray;
+                            }
+                    /*if (j == 0 || i == 0)
+                    {
+                        button.BackColor = Color.Gray;
+                        if (i == 0 && j > 0)
+                            button.Text = alphabet[j - 1].ToString();
+                        if (j == 0 && i > 0)
+                            button.Text = i.ToString();
+                    }*/
+                    myButtons[i, j] = button;
+                    this.Controls.Add(button);
+                }
+            }
+            Label map1 = new Label();
+            map1.Text = "Карта игрока";
+            map1.Location = new Point(mapSize * cellSize / 2, mapSize * cellSize + 10);
+            this.Controls.Add(map1);
+
+            Button startButton = new Button();
+            startButton.Text = "Начать";
+            startButton.Click += new EventHandler(Start);
+            startButton.Location = new Point(0, mapSize * cellSize + 20);
+            this.Controls.Add(startButton);
+        }
+
+        public void Start(object sender, EventArgs e)
+        {
+            isPlaying = true;
+            //myMap = ConfigureShips();
+        }
+
+        public int[,] ConfigureShips()
+        {
+            Button button = new Button();
+            GoogleApi myObject = new GoogleApi();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (myObject.ReadEntries()[i, j] != 0)
+                    {
+                        //myMap[i, j] = 1;
+                        button.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        button.BackColor = Color.Gray;
+                    }
+
+                }
+            }
+            return myMap;
+        }
+
+    }
+}
---
+++ GoogleApi.cs
@@ -0,0 +1,83 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using MyApp;
+using System;
+using System.IO;
+
+public class GoogleApi
+{
+    static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+    static readonly string ApplicationName = "SeaBattle";
+    static readonly string SpreadSheetId = "1eb2vaC45-prUvId_fjTEPqHbbeXYbooVciDVkXQBmkU";
+    static readonly string sheet = "Number1";
+    static SheetsService service;
+    static public int[,] array = new int[10, 10];
+
+    public GoogleApi()
+	{
+        Acsess();
+        array = ReadEntries();
+    }
+
+    public static void Acsess()
+    {
+        GoogleCredential credential;
+        using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
+        {
+            credential = GoogleCredential.FromStream(stream)
+                .CreateScoped(Scopes);
+        }
+
+        service = new SheetsService(new Google.Apis.Services.BaseClientService.Initializer()
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = ApplicationName,
+        });
+    }
+
+    public int[,] ReadEntries()
+    {
+        GoogleCredential credential;
+        using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
+        {
+            credential = GoogleCredential.FromStream(stream)
+                .CreateScoped(Scopes);
+        }
+
+        service = new SheetsService(new Google.Apis.Services.BaseClientService.Initializer()
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = ApplicationName,
+        });
+
+        var range = $"{sheet}!A1:J10"; //задаем диапазоны
+        var request = service.Spreadsheets.Values.Get(SpreadSheetId, range); //объект запроса
+
+        var response = request.Execute(); //объект ответа
+        var values = response.Values; //доступ к значниям
+        int[,] mass = new int[10, 10];
+        //if (values != null && values.Count > 0)
+        //{
+            int j = 0;
+            while (j < 10)
+            {            
+            foreach (var raw in values)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (raw[i] == "")
+                        {
+                            mass[j, i] = 0;
+                        }
+                        else
+                        {
+                            mass[j, i] = 1;
+                        }
+                    }
+                    j++;
+                }
+
+            }
+        return mass;
+    }
+}
